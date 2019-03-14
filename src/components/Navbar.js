4192bb00c8c420184c5f07109090cac42996ce13
[Chris Cook] <[cookc@allegheny.edu]>
2019-03-14 10:12:28 -0400

    Changed navbar to Country List

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -52,7 +52,7 @@ const Navbar = class extends React.Component {
                 About
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Country List
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
