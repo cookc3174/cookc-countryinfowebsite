7b1e9c30012fa058903435ed466533edadac7959
[Chris Cook] <[cookc@allegheny.edu]>
2019-04-08 15:49:19 -0400

    Added another import statement in products

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import Testimonials from '../components/Testimonials'
