0c923ec0dd8ff9970f30a01bc5178a9149567d0e
[Chris Cook] <[cookc@allegheny.edu]>
2019-04-11 16:23:12 -0400

    Removed date and buttons from tabs

--- src/components/BlogRoll2.js
+++ src/components/BlogRoll2.js
@@ -21,16 +21,6 @@ class BlogRoll2 extends React.Component {
                 <Link className="title has-text-primary is-size-4" to={post.fields.slug}>
                   {post.frontmatter.title}
                 </Link>
-                <span> &bull; </span>
-                <span className="subtitle is-size-5 is-block">{post.frontmatter.date}</span>
-              </p>
-              <p>
-                {post.excerpt}
-                <br />
-                <br />
-                <Link className="button" to={post.fields.slug}>
-                  Keep Reading →
-                </Link>
               </p>
               </article>
             </div>
