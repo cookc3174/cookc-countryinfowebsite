6274bbcee445466e7d1a1d2655b5d5d87867eae1
[Chris Cook] <[cookc@allegheny.edu]>
2019-04-15 15:44:52 -0400

    changed button name

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -87,7 +87,7 @@ export const IndexPageTemplate = ({
               <div className="columns">
               <div className="column is-12 has-text-centered">
                 <Link className="btn" to="/products">
-                  See all products
+                  See all countries
                 </Link>
                 </div>
               </div>
