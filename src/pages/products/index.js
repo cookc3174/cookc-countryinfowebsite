05adc33f76c03d8b5de8668a9955406466e4a9e6
[Chris Cook] <[cookc@allegheny.edu]>
2019-04-18 15:39:50 -0400

    Changed padding again

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -23,7 +23,7 @@ export default class BlogIndexPage extends React.Component {
                     boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
                     backgroundColor: '#f40',
                     color: 'white',
-                    padding: '0.5rem',
+                    padding: '0.1rem',
                   }}
                 >
                   Our World
